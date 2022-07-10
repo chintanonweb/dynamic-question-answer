889b897f5f7555d44c57c02c4fb51a184397bc79
chintan-albiorix <chintandhokai97@gmail.com>
2022-07-10 23:46:48 +0530

    code updated

---
+++ src/app/service/question.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionService {
+
+  constructor() { }
+
+  saveQuestion(questionData : any){
+    localStorage.removeItem('question')
+    localStorage.setItem('question' , JSON.stringify(questionData))
+  }
+
+  getQuestionData(){
+    return JSON.parse(localStorage.getItem('question')|| '{}')
+  }
+  
+}
